ae4cc3688113f041599782f852e1617e13ae3979
Ezgi <ezginurryigit@gmail.com>
2026-01-10 14:06:30 +0300

    Fix: reset quiz state and prevent answer carryover
    - Clear userAnswer and correctAnswer at the start of each quiz
    - Fix replay logic to avoid using answers from previous rounds
    - Improve random question selection stability

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ int main()
 	string line;
 	string question, optionA, optionB, optionC, optionD, userAnswer, correctAnswer;
 	char answer;
-	int count, trueNum=0, falseNum=0, emptyNum=0;
+	int count;
+	int trueNum=0, falseNum=0, emptyNum=0;
 	float total=0.0, score=0.0;
 	
 	ifstream file;
@@ -25,30 +26,43 @@ int main()
 	}
 	
 	cout<<"- - - QUIZ - - -"<<endl;
+	
 	int lineCount=0;
 	while(getline(file, line)){
 		lineCount++;
 	}
+	bool used[lineCount]={false};
+	int remainder=lineCount;
+	int howMany;
 	
-	do{
-	int questionNumber=0, howMany;
-	cout<<"Kaç soruluk yarýþma istiyorsunuz? (1-"<<lineCount<<"): ";
+	for(int q=1; q<=howMany; q++){
+	int questionNumber=0;
+	cout<<"Kaç soruluk yarýþma istiyorsunuz? (1-"<<remainder<<"): ";
 	cin>>howMany;
-	lineCount-=howMany;
+	remainder-=howMany;
 	cout<<endl;
 	cout<<"=== BÝLGÝ YARIÞMASI BAÞLADI ===\n"<<endl;
 	
 	file.close();
 	file.open("questions.txt");
-	int randomLine=(rand()%lineCount+1);
-	int currentLine=0;
+	
+	int randomLine;
+	do{
+	    randomLine=(rand()%lineCount+1);
+    }while(used[randomLine]==true);
+    
+    int currentLine=1;
 	while(getline(file, line)){
-		if(currentLine==randomLine) break;
-			currentLine++;
+		if(currentLine==randomLine){
+			used[randomLine]=true;
+			break;
+		}currentLine++;
 	}
 	
+	correctAnswer = userAnswer ="";
+	
 	while(getline(file, line)){
-		question="", optionA="", optionB="", optionC="", optionD="";
+		question = optionA = optionB = optionC = optionD ="";
 		count=0;
 			for(int j=0; j<line.length(); j++){
 				if(line[j]==';'){
@@ -78,18 +92,18 @@ int main()
 		cin>>answer;
 		cout<<endl;
 		
-		userAnswer+=answer;
+		userAnswer+=char(toupper(answer));
 		if(questionNumber==howMany) break;
 	    }
 	    
 	    cout<<"=== SONUÇ RAPORU ===\n"<<endl;
 	    count=1, trueNum=0, emptyNum=0, falseNum=0, total=0.0, score=0.0;
 	    for(int i=0; i<howMany; i++){
-	    	cout<<"Soru "<<count++<<" | Senin cevabin: "<<char(toupper(userAnswer[i]))<<" | Doðru cevap: "<<correctAnswer[i]<<endl;
+	    	cout<<"Soru "<<count++<<" | Senin cevabin: "<<userAnswer[i]<<" | Doðru cevap: "<<correctAnswer[i]<<endl;
 	    	
 	    	if((toupper(userAnswer[i]))==correctAnswer[i])
 	    	trueNum++;
-	    	else if((toupper(userAnswer[i]))=='X')
+	    	else if(userAnswer[i]=='X')
 	    	emptyNum++;
 	    	else
 	    	falseNum++;
@@ -113,8 +127,7 @@ int main()
 	    cout<<endl;
 	    else if(again=='H' || again=='h')
 	    break;
-	    
-	    } while(1);
+	    }
 	    
 	file.close();
 	return 0;
